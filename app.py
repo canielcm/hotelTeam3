cb6cb707711bbe6719c035d7ff1cd43f3a15319c
canielcm <ccastromaussa@gmail.com>
2021-10-16 18:59:45 -0500

    second commit

--- app.py
+++ app.py
@@ -6,6 +6,22 @@ app = Flask(__name__)
 def home_page():
     return render_template('home.html')
 
+@app.route("/home")
+def logged_page():
+    return render_template('loggedIn.html')
+
+@app.route("/feed")
+def feed_page():
+    return render_template('feedback.html')
+
+@app.route("/dashboard")
+def dashboard_page():
+    return render_template('dashboard.html')
+
+@app.route("/test")
+def test_page():
+    return render_template('test.html')
+
 @app.route('/about/<username>')
 def about_page(username):
     return f'<h1>About page of {username}</h1>'
