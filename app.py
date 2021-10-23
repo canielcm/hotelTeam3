760490205d3e5ff74a3aa4e7085556eab6dcf49e
nekomotsu9029 <gustavo.olmedo@softwareestrategico.com>
2021-10-23 08:28:31 -0500

    signIn & signUp

--- app.py
+++ app.py
@@ -1,12 +1,65 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session, url_for, redirect
 import sqlite3
+import hashlib
 
 app = Flask(__name__)
+app.secret_key = 'nekomotsu9029'
 
+@app.route('/signIn', methods=["POST"])
+def signin():
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+        #password = hashlib.sha256(password)
+        db = sqlite3.connect('hotel_db.db')
 
-@app.route("/")
-def home_page():
-    return render_template('home.html')
+        sentencia = "SELECT * FROM user where email='"+email+"' AND password='"+password+"'"
+        
+        cursor = db.cursor()
+        cursor.execute(sentencia)
+        data = cursor.fetchall()
+
+        db.close()
+
+        if(str(data) == "[]"):
+            message="Correo o Contraseña incorrectas :("
+            return render_template('home.html', session = {"login": 0, "message": message})
+        else:
+            session['email'] = email
+            return redirect(url_for('index'))
+
+@app.route('/signUp', methods=["POST"])
+def signup():
+    #conectamos con la bd
+    db = sqlite3.connect('hotel_db.db')
+    cursor = db.cursor()
+
+    name = request.form['name']
+    role = "usuario"
+    #request.form['role']
+    email = request.form['email']
+    password = request.form['password']
+    #password = hashlib.sha256(password)
+
+    sentencia = 'INSERT INTO user(name, role, email, password) VALUES("%s", "%s", "%s", "%s")' % (name, role, email, password)
+
+    cursor.execute(sentencia)
+    db.commit()
+
+    db.close()
+    session['email'] = email
+    return redirect(url_for('index'))
+
+@app.route('/')
+def index():
+    if 'email' in session:
+        email = session['email']
+        print('sesion encontrada, Email:'+email)
+        login = 1
+    else:
+        print('no hay sesion hdp')
+        login = 0
+    return render_template('home.html', session = {"login": login})
 
 
 @app.route("/home")
@@ -60,3 +113,6 @@ def rest_http():
 @app.route('/about/<username>')
 def about_page(username):
     return f'<h1>About page of {username}</h1>'
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
